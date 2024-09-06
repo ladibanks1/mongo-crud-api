1a4a0ed539c103ccd7cbcdf644922eb5468a1a22
Oladipupo <oladipupobankole24@gmail.com>
2024-09-06 07:21:48 +0100

    added endpoints links to the page

--- router/notfound.route.js
+++ router/notfound.route.js
@@ -1,6 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-router.all("*" , (req , res) => {
-    res.status(404).send("ERROR PAGE NOT FOUND")
-})
-module.exports = router
+router.all("*", (req, res) => {
+  res
+    .status(200)
+    .send(
+      'Endpoints:\n Get Account: https://mongo-crud-api-bbel.onrender.com/getAccount/:id \n Create Account: https://mongo-crud-api-bbel.onrender.com/createAccount/ \n  Delete Account : https://mongo-crud-api-bbel.onrender.com/deleteAccount/:id \n  Update Account : https://mongo-crud-api-bbel.onrender.com/updateAccount/:id \n'
+    );
+});
+module.exports = router;
