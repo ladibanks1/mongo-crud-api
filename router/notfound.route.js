d860afbac2a2401ac633e5ab394d53a99cd99093
Oladipupo <oladipupobankole24@gmail.com>
2024-09-06 07:38:11 +0100

    update the endpoints response to html

--- router/notfound.route.js
+++ router/notfound.route.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 router.all("*", (req, res) => {
-  res
-    .status(200)
-    .send(
-      'Endpoints:\n Get Account: https://mongo-crud-api-bbel.onrender.com/getAccount/:id \n Create Account: https://mongo-crud-api-bbel.onrender.com/createAccount/ \n  Delete Account : https://mongo-crud-api-bbel.onrender.com/deleteAccount/:id \n  Update Account : https://mongo-crud-api-bbel.onrender.com/updateAccount/:id \n'
-    );
+  res.status(200).send(
+    `<h1>Endpoints:</h1>
+       <p>Get Account:<a href="https://mongo-crud-api-bbel.onrender.com/getAccount/">/getAccount/:account_number</a></p>
+       </p> 
+       <p>Create Account: 
+        <a href="https://mongo-crud-api-bbel.onrender.com/createAccount">/createAccount</a>
+       </p>
+       <p>Delete Account: 
+        <a href="https://mongo-crud-api-bbel.onrender.com/deleteAccount/">/deleteAccount/:account_number</a>
+       <p>Update Account:
+        <a href="https://mongo-crud-api-bbel.onrender.com/updateAccount/">/updateAccount/:account_number</a>
+       </p>
+       `
+  );
 });
 module.exports = router;
